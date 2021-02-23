fc79e445ac9137f271e543a9cc21a8233d42a4ee
Jerwyn Alqueza <alqueza@ualberta.ca>
2021-02-23 15:10:13 -0800

    fix requirments

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-import django-on-heroku
+import django_on_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
